3f0138cb441f843454bae60ac4cbd24b317ad56e
Utkarsh Shukla <231165@iiitt.ac.in>
2026-03-30 01:01:22 +0530

    Added Kubernetes manifests

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGO_URI);
 
 const ItemSchema = new mongoose.Schema({
     name: String
